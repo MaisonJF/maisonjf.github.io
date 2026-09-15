18c70105f5ede21d2c6a1233006458cfb6f1bb44
Maison JF™ <maisonjf@proton.me>
2026-09-15 18:25:37 +0100

    Centralize live ebook commerce registry for scalable releases

---
+++ functions/_lib/ebooks-live.js
@@ -0,0 +1,21 @@
+export const EBOOKS = {
+  turista: {
+    slug: 'virgulas-do-destino-o-turista',
+    title: 'Vírgulas do Destino: O Turista',
+    priceId: 'price_1UEoZP5H3wYRPmPVLTG7nmwl',
+    unitCents: 299,
+    key: 'ebooks/virgulas-do-destino-o-turista.pdf',
+    filename: 'Virgulas-do-Destino-O-Turista.pdf'
+  },
+  meandros: {
+    slug: 'virgulas-do-destino-meandros-da-vida',
+    title: 'Vírgulas do Destino: Meandros da Vida',
+    priceId: 'price_1UEoWC5H3wYRPmPVFiVh4xT8',
+    unitCents: 499,
+    key: 'ebooks/virgulas-do-destino-meandros-da-vida.pdf',
+    filename: 'Virgulas-do-Destino-Meandros-da-Vida.pdf'
+  }
+};
+
+export const EBOOK_IDS = Object.keys(EBOOKS);
+export const isEbookId = id => Object.prototype.hasOwnProperty.call(EBOOKS, id);
